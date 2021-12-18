a53e6f3cc1e645d53c6391c83ab2dd9a79ff4bef
ธนภรณ์ พึ่งแพง <95395652+Tanaporn333@users.noreply.github.com>
2021-12-18 14:39:33 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-/*คำสั่งให้ปุ่มเบอร์เก้อทำงาน*/
-let mainNav = document.getElementById('js-menu');
-let navBarToggle = document.getElementById('js-nav-toggle');
-
-navBarToggle.addEventListener("click",function(){
-  mainNav.classList.toggle('active');
-});
-
-
